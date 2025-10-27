eb899a6305def2ad264cb81bea24cdf96735c84b
sanjupatel799 <bandarisanjaykumar8@gmail.com>
2025-10-27 13:02:57 +0530

    symbol modified

--- buttonexternal.js
+++ buttonexternal.js
@@ -2,7 +2,7 @@ function agecal(){
     let age = document.getElementById("age").value;
     alert("checking vote elgiblity");
 
-    if (age = 18) {
+    if (age >= 18) {
         document.getElementById("vote").innerHTML = "age "+age+" Eligible for Vote";
 
     }
