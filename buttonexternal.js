aa628603a84c84da481cb2fd9467d3cda111fa72
sanjupatel799 <bandarisanjaykumar8@gmail.com>
2025-10-27 10:45:35 +0530

    conditional js

---
+++ buttonexternal.js
@@ -0,0 +1,12 @@
+function agecal(){
+    let age = document.getElementById("age").value;
+    alert("checking vote elgiblity");
+
+    if (age = 18) {
+        document.getElementById("vote").innerHTML = "age "+age+" Eligible for Vote";
+
+    }
+    else {
+        document.getElementById("vote").innerHTML = "age "+age+" Not Eligible for Vote";
+    }
+}
